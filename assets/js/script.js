b7ac5d0712099783535f4ebab2b6574fae871932
Mateus Medeiros <mateusdesousamedeiros@hotmail.com>
2017-01-28 12:05:35 -0200

    Corrigindo texto - sem resultados

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ SimpleJekyllSearch({
   resultsContainer: document.getElementById('results-container'),
   json: '/search.json',
   searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
-  noResultsText: 'Sem resultados'
+  noResultsText: '<p>Sem resultados</p>'
 })
 
 $(window).resize(function(){
