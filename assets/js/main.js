2e57259b91955a5fec114505fd34ac7cfe713fe3
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2017-04-08 00:48:42 -0300

    Adaptando menu para mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,23 +7,21 @@ $('a.scroll__btn').smoothScroll();
 // Search
 // ==========================================================================
 // Variables
-var btnOpen = document.getElementById('search-open');
+var btnSearch = document.getElementById('search-open');
 var searchContainer = document.getElementById('search-wrapper');
 
 // Open Search Container
 function openSearch() {
   searchContainer.classList.remove('is-hidden'); 
   searchContainer.classList.add('is-visibly'); 
-
-  btnOpen.setAttribute('aria-expanded', true);
+  btnSearch.setAttribute('aria-expanded', true);
 }
 
 // Close Search Container
 function closeSearch() {
   searchContainer.classList.remove('is-visibly'); 
   searchContainer.classList.add('is-hidden'); 
-
-  btnOpen.setAttribute('aria-expanded', false);
+  btnSearch.setAttribute('aria-expanded', false);
 }
 
 // Simple Jekyll Search
@@ -34,3 +32,48 @@ SimpleJekyllSearch({
   searchResultTemplate: '<li class="navbar__search__results__item"><a class="navbar__search__results__item__link" href="{url}" title="{desc}">{title}</a></li>',
   noResultsText: '<p class="navbar__search__results--no-results">Nenhum post foi encontrado! :(</p>'
 })
+
+// ==========================================================================
+// Menu
+// ==========================================================================
+// Variables
+var btnMenu = document.getElementById('menu-open');
+var menuContainer = document.getElementById('menu-list');
+var btnDropdown = document.getElementById('open-dropdown');
+var menuDropdown = document.getElementById('menu-dropdown');
+
+// Open Menu Container
+function openMenu() {
+  menuContainer.classList.add('is-visibly'); 
+  btnMenu.setAttribute('aria-expanded', true);
+}
+
+// Close Menu Container
+function closeMenu() {
+  menuContainer.classList.remove('is-visibly'); 
+  btnMenu.setAttribute('aria-expanded', false);
+}
+
+// Open/Close Dropdown Menu
+function controlDropdown() {
+  if(!menuDropdown.classList.contains("is-visibly")) {
+    menuDropdown.classList.remove('is-hidden');
+    menuDropdown.classList.add('is-visibly');
+    btnDropdown.setAttribute('aria-expanded', true);
+  } else if(menuDropdown.classList.contains("is-visibly")) {
+    menuDropdown.classList.remove('is-visibly');
+    menuDropdown.classList.add('is-hidden');
+    btnDropdown.setAttribute('aria-expanded', false);
+  }
+}
+
+// ==========================================================================
+// Remove CSS
+// ==========================================================================
+window.addEventListener('resize', function(){
+  if (window.innerWidth > 768) {
+    menuContainer.classList.remove('is-visibly');  
+    menuDropdown.classList.remove('is-visibly');
+    menuDropdown.classList.remove('is-hidden');
+  }
+});
