4f78d55f04f3847d83099add6b4bb83f17101278
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2020-09-22 22:25:17 -0300

    Up: form contato

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,63 @@ $('a[href^="#"]').on('click', function(event) {
 
     return false;
 });
+
+$('#tel').mask("(99) 9999-99999")
+.focusout(function (event) {  
+    var target, phone, element;  
+    target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+    phone = target.value.replace(/\D/g, '');
+    element = $(target);  
+    element.unmask();  
+    if(phone.length > 10) {  
+        element.mask("(99) 99999-9999");  
+    } else {  
+        element.mask("(99) 9999-9999");  
+    }  
+});
+
 $(document).ready(function() {
     $('#cookieConsent').cookieConsent();
 });
 
+$('.slider').slick({
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:'<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="../assets/img/lp/prev.svg" class="icon"></button>',
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="../assets/img/lp/next.svg" class="icon"></button>'
+});
+
+$(document).ready(function() {
+    $('#tel').mask("(99) 9999-99999")
+    .focusout(function (event) {  
+        var target, phone, element;  
+        target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+        phone = target.value.replace(/\D/g, '');
+        element = $(target);  
+        element.unmask();  
+        if(phone.length > 10) {  
+            element.mask("(99) 99999-9999");  
+        } else {  
+            element.mask("(99) 9999-9999");  
+        }  
+    });
+    $('#cookieConsent').cookieConsent();
+});
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+    return false;
+});
 function controlMenu() {
   const btn = document.querySelector('#btn-menu');
   const menu = document.querySelector('#menu');
